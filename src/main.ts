85a711a806673ca4ff7e93911476d06511ef6c64
Maya Rajan <mayarajan222@gmail.com>
2026-01-15 22:46:51 +0000

    jan 15 updates

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,32 @@ import "./style.css";
 import Doodlebot from "../release/doodlebot/Doodlebot";
 import LineArrayFollowing from "../release/LineArrayFollowing";
 import { getBLEDeviceWithUartService } from "../release/doodlebot/ble";
+import { type SensorKey, sensor } from "../release/doodlebot/enums";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
       <button id="start">
-          Connect and start
+          Connect
+      </button>
+  </div>
+  <div>
+      <button id="calibrate">
+          Calibrate
+      </button>
+  </div>
+  <div>
+      <button id="lineFollow">
+          Follow Line
       </button>
   </div>
 `;
+
+let doodlebot: Doodlebot;
+
+const calibrate = async (element: HTMLButtonElement) =>
+  element.addEventListener("click", async () => {
+    doodlebot.sendBLECommand("m", "c");
+  });
 
 const setup = async (element: HTMLButtonElement) =>
   element.addEventListener("click", async () => {
@@ -24,15 +42,28 @@ const setup = async (element: HTMLButtonElement) =>
     if (!topLevelDomain.endsWith(".direct.mitlivinglab.org"))
       topLevelDomain += ".direct.mitlivinglab.org";
 
-    const doodlebot = new Doodlebot();
+    doodlebot = new Doodlebot();
     doodlebot.topLevelDomain.resolve(topLevelDomain);
     doodlebot.bleDevice.resolve(result);
 
+    for (const key of Object.keys(sensor)) {
+      doodlebot.disableSensor(key as SensorKey); // don't await
+    }
+    window.addEventListener("pagehide", () => {
+      for (const key of Object.keys(sensor)) {
+        doodlebot.disableSensor(key as SensorKey); // don't await
+      }
+    });
+  });
+
+const lineFollow = async (element: HTMLButtonElement) =>
+  element.addEventListener("click", async () => {
+    console.log(await doodlebot.getSensorReading("line"));
     const lineFollower = new LineArrayFollowing(
       2, // Kp
       200, // base speed
       500, // max speed
-      200, // min speed
+      0, // min speed
       doodlebot.sendBLECommand.bind(doodlebot),
       doodlebot.getSensorReading.bind(doodlebot)
     );
@@ -45,10 +76,10 @@ const setup = async (element: HTMLButtonElement) =>
       const status = await lineFollower.getLineStatus();
       switch (status) {
         case "left of line":
-          await lineFollower.turnLeft();
+          await lineFollower.turnRight();
           break;
         case "right of line":
-          await lineFollower.turnRight();
+          await lineFollower.turnLeft();
           break;
         case "on the line":
           await lineFollower.goStraight();
@@ -61,3 +92,5 @@ const setup = async (element: HTMLButtonElement) =>
   });
 
 setup(document.querySelector<HTMLButtonElement>("#start")!);
+calibrate(document.querySelector<HTMLButtonElement>("#calibrate")!);
+lineFollow(document.querySelector<HTMLButtonElement>("#lineFollow")!);
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    nodePolyfills({
+      include: ["util"],
+      globals: {
+        global: true,
+      },
+    }),
+  ],
+});
